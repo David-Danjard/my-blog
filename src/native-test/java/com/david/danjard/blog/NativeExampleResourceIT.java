f1281b74270f97fae1c1cfbc2bb9cebfe636b9ca
david.danjard@gmail.com <A45y22s@>
2020-05-01 15:44:33 +0200

    initial commit

---
+++ src/native-test/java/com/david/danjard/blog/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package com.david.danjard.blog;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
